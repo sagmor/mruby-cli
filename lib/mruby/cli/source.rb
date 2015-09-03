d15178b096942895209ca9fef76fefbaccc56eed
Seba Gamboa <me@sagmor.com>
2015-09-03 15:49:02 -0300

    Improved mruby/source load error

--- lib/mruby/cli/source.rb
+++ lib/mruby/cli/source.rb
@@ -1,12 +1,25 @@
 module MRuby::CLI
   # Wrapper around MRuby::Source module to stop execution if MRuby's source is not available
   module Source
+    ERROR_MESSAGE = <<-EOM
+
+Error:
+      To execute this action you need a copy of MRuby's source.
+
+      Add this to your Gemfile:
+
+          gem "mruby-source", github: 'sagmor/mruby', branch: 'gem'
+
+      Run bundle install and re-run your last command
+
+    EOM
+
     def self.path
       require 'mruby/source'
 
       MRuby::Source.path
     rescue LoadError
-      puts "To execute this action you need a copy of MRuby's source!"
+      puts ERROR_MESSAGE
       exit 1
     end
   end
